5afae3a9566c5ac0f510fda86d834977ef6df832
xuqingfeng <qingfeng.xu.contractor@hilton.com>
2026-07-14 16:52:54 +0800

    Add upload metrics and wifi watcher

--- Spoons/SpeedMenu.spoon/init.lua
+++ Spoons/SpeedMenu.spoon/init.lua
@@ -16,6 +16,28 @@ obj.license = "MIT - https://opensource.org/licenses/MIT"
 
 local logger = hs.logger.new("speed", "debug")
 
+local function trim(text)
+    return (text or ""):gsub("^%s+", ""):gsub("%s+$", "")
+end
+
+local function formatSpeed(bytes)
+    if bytes / 1024 > 1024 then
+        return string.format("%6.2f MB/s", bytes / 1024 / 1024)
+    end
+    return string.format("%6.2f KB/s", bytes / 1024)
+end
+
+local function getInterfaceBytes(interface)
+    local output = trim(hs.execute('netstat -ibn | grep -e ' .. interface .. ' -m 1'))
+    if output == "" then return 0, 0 end
+
+    local fields = {}
+    for field in output:gmatch("%S+") do
+        table.insert(fields, field)
+    end
+    return tonumber(fields[7]) or 0, tonumber(fields[10]) or 0
+end
+
 local function isWifiInterface(interface)
     if not interface then return false end
     return hs.fnutils.contains(hs.wifi.interfaces() or {}, interface)
@@ -28,23 +50,6 @@ local function detectActiveInterface()
 
     local connectionType = isWifiInterface(interface) and "wifi" or "ethernet"
     return interface, connectionType
-end
-
-local function executeNumber(cmd)
-    local output = hs.execute(cmd)
-    return tonumber(output) or 0
-end
-
-local function requestLocationPermissionIfNeeded(self)
-    -- macOS Sonoma+ requires Location Services to read WiFi SSID
-    if not hs.location or not hs.location.servicesEnabled() then return end
-    if self.locationRequested then return end
-    self.locationRequested = true
-    hs.location.start()
-    hs.timer.doAfter(2, function()
-        hs.location.stop()
-        self:rescan()
-    end)
 end
 
 local function getSSID(interface)
@@ -67,62 +72,47 @@ local function getSSID(interface)
     return ssid
 end
 
-function obj:init()
-    self.menubar = hs.menubar.new()
-    self.menubar:autosaveName("xuqingfeng.speedmenu")
-    requestLocationPermissionIfNeeded(self)
-    self:rescan()
+local function requestLocationPermissionIfNeeded(self)
+    -- macOS Sonoma+ requires Location Services to read WiFi SSID
+    if not hs.location or not hs.location.servicesEnabled() then return end
+    if self.locationRequested then return end
+    self.locationRequested = true
+    hs.location.start()
+    hs.timer.doAfter(2, function()
+        hs.location.stop()
+        self:rescan()
+    end)
+end
 
-    if self.rescanTimer then
-        self.rescanTimer:stop()
-        self.rescanTimer = nil
+local function stopSpeedTimer(self)
+    if self.timer then
+        self.timer:stop()
+        self.timer = nil
     end
-    -- rescan network interface every 6 hours
-    self.rescanTimer = hs.timer.doEvery(21600, function() self:rescan() end)
 end
 
 local function data_diff(self)
-    local in_seq = executeNumber(self.instr)
-    local out_seq = executeNumber(self.outstr)
+    local in_seq, out_seq = getInterfaceBytes(self.interface)
     local in_diff = in_seq - self.inseq
     local out_diff = out_seq - self.outseq
-    if in_diff/1024 > 1024 then
-        self.kbin = string.format("%6.2f", in_diff/1024/1024) .. ' MB/s'
-    else
-        self.kbin = string.format("%6.2f", in_diff/1024) .. ' KB/s'
-    end
-    if out_diff/1024 > 1024 then
-        self.kbout = string.format("%6.2f", out_diff/1024/1024) .. ' MB/s'
-    else
-        self.kbout = string.format("%6.2f", out_diff/1024) .. ' KB/s'
-    end
-    -- local disp_str = '↓' .. self.kbin .. ' ↑'.. self.kbout
-    -- FIXME: self.kbout not accurate
-    local disp_str = '↓' .. self.kbin
+
+    local disp_str = '↓' .. formatSpeed(in_diff) .. ' ↑' .. formatSpeed(out_diff)
     self.disp_str = hs.styledtext.new(disp_str, {font={size=12.0}})
     self.menubar:setTitle(self.disp_str)
     self.inseq = in_seq
     self.outseq = out_seq
 end
 
---- SpeedMenu:rescan()
---- Method
---- Redetect the active interface and redraw everything.
----
-
-function obj:rescan()
-
-    self.interface, self.connectionType = detectActiveInterface()
-    logger.df("I! interface: %s (%s)", self.interface, self.connectionType or "unknown")
-
+local function buildMenuItems(self)
     local menuitems_table = {}
+
     if self.interface then
-        -- Inspect active interface and create menuitems
-        local interface_detail = hs.network.interfaceDetails(self.interface)
+        local interface_detail = hs.network.interfaceDetails(self.interface) or {}
         local connectionLabel = self.connectionType == "wifi" and "WiFi" or "Ethernet"
         table.insert(menuitems_table, {
             title = "Connection: " .. connectionLabel .. " (" .. self.interface .. ")",
         })
+
         local ssid = self.connectionType == "wifi" and getSSID(self.interface)
         if ssid then
             table.insert(menuitems_table, {
@@ -147,36 +137,67 @@ function obj:rescan()
                 fn = function() hs.pasteboard.setContents(ipv6) end
             })
         end
-        local macaddr = hs.execute('ifconfig ' .. self.interface .. ' | grep ether | awk \'{print $2}\'')
+        local macaddr = trim(hs.execute('ifconfig ' .. self.interface .. ' | grep ether | awk \'{print $2}\''))
         table.insert(menuitems_table, {
             title = "MAC Addr: " .. macaddr,
             tooltip = "Copy MAC Address to clipboard",
             fn = function() hs.pasteboard.setContents(macaddr) end
         })
-        -- Start watching the netspeed delta
-        self.instr = 'netstat -ibn | grep -e ' .. self.interface .. ' -m 1 | awk \'{print $7}\''
-        self.outstr = 'netstat -ibn | grep -e ' .. self.interface .. ' -m 1 | awk \'{print $10}\''
-
-        self.inseq = executeNumber(self.instr)
-        self.outseq = executeNumber(self.outstr)
-
-        if self.timer then
-            self.timer:stop()
-            self.timer = nil
-        end
-        self.timer = hs.timer.doEvery(1, function() data_diff(self) end)
-    else
-        if self.timer then
-            self.timer:stop()
-            self.timer = nil
-        end
-        self.menubar:setTitle("⚠︎")
     end
+
     table.insert(menuitems_table, {
         title = "Rescan Network Interfaces",
         fn = function() self:rescan() end
     })
-    self.menubar:setMenu(menuitems_table)
+
+    return menuitems_table
+end
+
+function obj:init()
+    self.menubar = hs.menubar.new()
+    self.menubar:autosaveName("xuqingfeng.speedmenu")
+    requestLocationPermissionIfNeeded(self)
+    self:rescan()
+
+    if self.rescanTimer then
+        self.rescanTimer:stop()
+        self.rescanTimer = nil
+    end
+    -- rescan network interface every 6 hours
+    self.rescanTimer = hs.timer.doEvery(21600, function() self:rescan() end)
+
+    if self.wifiWatcher then
+        self.wifiWatcher:stop()
+        self.wifiWatcher = nil
+    end
+    self.wifiWatcher = hs.wifi.watcher.new(function()
+        self:rescan()
+    end):start()
+end
+
+--- SpeedMenu:rescan()
+--- Method
+--- Redetect the active interface and redraw everything.
+---
+function obj:rescan()
+    local interface, connectionType = detectActiveInterface()
+    local interfaceChanged = interface ~= self.interface
+    self.interface = interface
+    self.connectionType = connectionType
+    logger.df("I! interface: %s (%s)", self.interface, self.connectionType or "unknown")
+
+    if self.interface then
+        if interfaceChanged or not self.timer then
+            self.inseq, self.outseq = getInterfaceBytes(self.interface)
+            stopSpeedTimer(self)
+            self.timer = hs.timer.doEvery(1, function() data_diff(self) end)
+        end
+    else
+        stopSpeedTimer(self)
+        self.menubar:setTitle("⚠︎")
+    end
+
+    self.menubar:setMenu(buildMenuItems(self))
 end
 
 return obj
